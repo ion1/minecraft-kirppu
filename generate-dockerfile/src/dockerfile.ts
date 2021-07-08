b24662bc0aa3f11fcf268f21afe6738542a2845f
Johan Kiviniemi <devel@johan.kiviniemi.name>
2021-07-08 20:45:36 +0300

    generate-dockerfile: Report all errors, not just the first one

--- generate-dockerfile/src/dockerfile.ts
+++ generate-dockerfile/src/dockerfile.ts
@@ -100,45 +100,46 @@ async function dockerfileEnvironment(
   minecraftVersion: string,
   frontmatterMods: Record<string, FrontmatterMod>
 ): Promise<DockerfileEnvironment> {
+  const fabricInstallerURLP = fabric.getInstallerURL();
+  const fabricLoaderVersionP = fabric.getLoaderVersion(minecraftVersion);
+  const dockerFileModPs = Object.entries(frontmatterMods).map(
+    async ([downloadId, mod]) => {
+      const { id } = mod;
+      let download: Download | null = null;
+
+      if ("modrinth" in id) {
+        download = await modrinth.getModDownload(id.modrinth, minecraftVersion);
+      } else if ("curseforge" in id) {
+        download = await curseforge.getModDownload(
+          id.curseforge,
+          minecraftVersion
+        );
+      } else {
+        const impossible: never = id;
+        throw new Error(
+          `Impossible frontmatter mod ID: ${JSON.stringify(impossible)}`
+        );
+      }
+
+      if (download != null) {
+        return {
+          id: downloadId,
+          urls: download.urls,
+          filename: download.filename,
+        };
+      } else {
+        throw new Error(
+          `No compatible files for ${downloadId} ${JSON.stringify(mod)}`
+        );
+      }
+    }
+  );
+
   const [fabricInstallerURL, fabricLoaderVersion, dockerfileMods] =
-    await Promise.all([
-      fabric.getInstallerURL(),
-      fabric.getLoaderVersion(minecraftVersion),
-      Promise.all(
-        Object.entries(frontmatterMods).map(async ([downloadId, mod]) => {
-          const { id } = mod;
-          let download: Download | null = null;
-
-          if ("modrinth" in id) {
-            download = await modrinth.getModDownload(
-              id.modrinth,
-              minecraftVersion
-            );
-          } else if ("curseforge" in id) {
-            download = await curseforge.getModDownload(
-              id.curseforge,
-              minecraftVersion
-            );
-          } else {
-            const impossible: never = id;
-            throw new Error(
-              `Impossible frontmatter mod ID: ${JSON.stringify(impossible)}`
-            );
-          }
-
-          if (download != null) {
-            return {
-              id: downloadId,
-              urls: download.urls,
-              filename: download.filename,
-            };
-          } else {
-            throw new Error(`No compatible files for ${downloadId} ${JSON.stringify(
-              mod
-            )}`);
-          }
-        })
-      ),
+    await gatherErrors([
+      fabricInstallerURLP,
+      fabricLoaderVersionP,
+      gatherErrors(dockerFileModPs),
     ]);
 
   return {
@@ -147,4 +148,39 @@ async function dockerfileEnvironment(
     fabricLoaderVersion,
     mods: dockerfileMods,
   };
+}
+
+async function gatherErrors<T1, T2, T3>(
+  promises: [Promise<T1>, Promise<T2>, Promise<T3>]
+): Promise<[T1, T2, T3]>;
+async function gatherErrors<T>(promises: Iterable<Promise<T>>): Promise<T[]>;
+async function gatherErrors(
+  promises: Iterable<Promise<unknown>>
+): Promise<unknown[]> {
+  const results = await Promise.allSettled(promises);
+  const fulfilled = [];
+  const rejected = [];
+
+  for (const result of results) {
+    if (result.status === "fulfilled") {
+      fulfilled.push(result.value);
+    } else if (result.status === "rejected") {
+      rejected.push(result.reason);
+    }
+  }
+
+  if (rejected.length > 0) {
+    throw new AggregateError(
+      rejected,
+      rejected
+        .map((err) =>
+          "message" in err && typeof err.message === "string"
+            ? err.message
+            : err
+        )
+        .join("\n")
+    );
+  }
+
+  return fulfilled;
 }
